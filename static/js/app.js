310821d1a9f89d01a906dd6f9c0de8319fde63d4
Aaron Miller <miller_aaron@hotmail.com>
2022-06-26 13:23:42 -0700

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import the data from data.js
 
 const tableData = data;
@@ -41,48 +40,4 @@ d3.selectAll("#filter-btn").on("click", handleClick);
 
 
 
-=======
-// import the data from data.js
-
-const tableData = data;
-
-// Reference the HTML table using d3
-
-var tbody = d3.select("tbody");
-
-function buildTable(data) {
-
-    tbody.html("");
-
-    data.forEach((dataRow) => {
-
-        let row = tbody.append("tr");
-
-        Object.values(dataRow).forEach((val) => {
-
-          let cell = row.append("td");
-
-          cell.text(val);
-          }
-        );
-    });
-}
-
-function handleClick() {
-    let date = d3.select("#datetime").property("value");
-    let filteredData = tableData;
-
-    if (date) {
-        filteredData = filteredData.filter(row => row.datetime === date);
-    }
-
-    buildTable(filteredData);
-};
-
-
-d3.selectAll("#filter.btn").on("click", handleClick);
-
-
-
->>>>>>> 6844842ff74a683356ce7322e24873c552f8cbaf
 buildTable(tableData);
