3e5cd4f777df819317560e15a712321c85139367
Aaron Miller <miller_aaron@hotmail.com>
2022-06-26 17:37:21 -0700

    finished app.js

--- static/js/app.js
+++ static/js/app.js
@@ -57,12 +57,15 @@ function updateTable() {
   // 8. Set the filtered data to the tableData.
   let filteredData = tableData;
 
+
+
   // 9. Loop through all of the filters and keep any data that
   // matches the filter values
-  for (var i = 0; i < filters.length; i++) {
-    filteredData = filteredData.filter(row => row.filterId === changedElement);
-  }
-    
+
+  Object.entries(filters).forEach(([key, value]) => {
+    filteredData = filteredData.filter(row => row[key] === value)
+  })
+ 
   // 10. Finally, rebuild the table using the filtered data
   buildTable(filteredData);
 }
